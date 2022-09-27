78f16cf443484a11c06d3e4d18d661243e44f9ba
jakekirsch2 <jakekirsch11@gmail.com>
2022-09-26 21:39:44 -0500

    adding latest

--- dags/config_dag.py
+++ dags/config_dag.py
@@ -19,7 +19,7 @@ def create_dag(dag_id,
             location='us-central1',
             cluster_name='us-central1-kirsch-data-pla-dd54cf32-gke',
             namespace='default',
-            image='us-central1-docker.pkg.dev/kirsch-data-platform/docker-repository/add-hc-pricing-data:984e509',
+            image='us-central1-docker.pkg.dev/kirsch-data-platform/docker-repository/add-hc-pricing-data:latest',
             name='add-hc-pricing-data',
             cmds=['python', 'main.py', '--company', 'humana', '--file_url', 'https://developers.humana.com/Resource/DownloadPCTFile?fileType=innetwork&fileName=2022-08-25_1_in-network-rates_000000000000.csv.gz', '--file_name', 'test_2022-08-25_1_in-network-rates_000000000000.csv.gz']
         )
